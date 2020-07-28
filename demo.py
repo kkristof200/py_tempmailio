46c919967212e725aec99c4dd10b578f076a522e
Kristof <kristof.kovacs@telenav.com>
2020-07-28 13:02:06 +0300

    new version

---
+++ demo.py
@@ -0,0 +1,21 @@
+import time
+
+from tempmailio import *
+
+mail = TempMailI0(
+    user_name='test',
+    domain=Domain.cloudmail()
+)
+
+success = mail.create_email()
+
+print(mail.email_address)
+
+while True:
+    inbox = mail.get_inbox()
+
+    if len(inbox) > 0:
+        inbox[0].jsonprint()
+        exit(0)
+
+    time.sleep(2.5)
